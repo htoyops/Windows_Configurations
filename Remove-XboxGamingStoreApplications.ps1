eba8077e74069d027cc1a5b62c74691d94cfcf92
htoyops <tolga.oy@gmail.com>
2022-05-15 00:39:04 +0300

    Xbox Applications Remover is Added
    Contains a Powershell Script To Uninstall Xbox Applications

---
+++ Remove-XboxGamingStoreApplications.ps1
@@ -0,0 +1,5 @@
+Get-AppxPackage -Name "Microsoft.GamingApp" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "Microsoft.GamingServices" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "Microsoft.Xbox.TCUI" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "Microsoft.XboxGamingOverlay" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "Microsoft.XboxIdentityProvider" | Remove-AppxPackage -AllUsers
